bf94579322873714e68a8668de3f2a34da302a7f
Osyfe2 <54218896+Osyfe2@users.noreply.github.com>
2022-08-05 11:32:38 +0200

    window icons

--- src/lib.rs
+++ src/lib.rs
@@ -167,6 +167,19 @@ impl Context
         self.window.set_title(title);
     }
 
+    #[cfg(windows)]
+    pub fn set_icon(&mut self, window_icon: Option<winit::window::Icon>)
+    {
+        self.window.set_window_icon(window_icon);
+    }
+
+    #[cfg(windows)]
+    pub fn set_icon_raw(&mut self, colors: Vec<u8>, (width, height): (u32, u32))
+    {
+        let icon = winit::window::Icon::from_rgba(colors, width, height);
+        self.set_icon(icon.ok());
+    }
+
     #[inline]
     pub fn window_dims(&self) -> (u32, u32)
     {
